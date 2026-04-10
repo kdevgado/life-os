a5764026c65fa60d12debdbbc34916867ff3763e
s3841649 <s3841649@student.rmit.edu.au>
2026-04-10 10:59:03 +1000

    fix(pwa): disable index.html navigation fallback in service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
         },
         workbox: {
           globPatterns: ["**/*.{html,js,css,png,svg,ico,webp}"],
+          navigateFallback: null,
         },
         manifest: {
           id: "/",
